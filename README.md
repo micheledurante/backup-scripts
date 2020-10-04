2033a0a287d0a91e6cc848ad9d55120c3730bd7d
micheled <me@thisisdurante.com>
2020-10-05 00:27:01 +0200

    Add dns resolver checks and print out in case of error

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Scripts-pluto
 A collection of scripts and other misc items related to the Pluto box. 
 
-## Deployment
+## Deployments
 `monthly-cleanup.sh` is located at `/opt/scripts-pluto`
-The script is defined `/var/spool/cron/crontabs/root` as `00 01 * * * /opt/scripts-pluto/scripts/backup-mail.sh`
+The script is defined `/var/spool/cron/crontabs/root` as:
+
+`00 01 * * * /opt/scripts-pluto/scripts/backup-mail.sh`
+
+##Telegram Bots
+Follow instructions here https://blog.bj13.us/2016/09/06/how-to-send-yourself-a-telegram-message-from-bash.html.
+
+###Pluto Monitoring
+- Bot name: `micheled_pluto`
+- Bot user name: `micheled_pluto_bot`
--- rust/dns_checker/src/main.rs
+++ rust/dns_checker/src/main.rs
@@ -1,4 +1,27 @@
-fn main() -> Result<(), std::io::Error> {
-    println!("Hello, world!");
+use std::io::Error;
+use trust_dns_resolver::Resolver;
+
+fn validate_domains() {
+    let domains = vec![
+        "annikaschall.com",
+        "micheledurante.co.uk",
+        "micheledurante",
+        "micheledurante.com",
+    ];
+
+    let resolver = Resolver::from_system_conf().unwrap();
+
+    for domain in domains {
+        let response = resolver.lookup_ip(domain);
+
+        if response.is_err() {
+            println!("{:?}", response.unwrap_err().kind());
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    validate_domains();
+
     Ok(())
 }
