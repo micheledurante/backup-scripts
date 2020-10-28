e9cc23b7bc13a64db2c7d9f3b3aae41df33bedaa
micheled <me@thisisdurante.com>
2020-10-29 00:49:27 +0100

    Implement telegram messages

--- rust/dns_checker/src/main.rs
+++ rust/dns_checker/src/main.rs
@@ -1,27 +1,35 @@
 /**
- * By default, Rust variables are passed by "copy" or "move". You need to expressly use ref in
- * function declarations!!!
+ * By default, Rust variables are passed by "copy" or "move" (depends on type). Remember to
+ * expressly use ref in function declarations!!!
  *
  * @see https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html
  */
+use std::env;
 use std::io::Error;
 use trust_dns_resolver::Resolver;
+use ureq;
+
+/// Send a message to the defined BOT by the env vars.
+pub fn send_telegram_message(text: String) {
+    ureq::post(&format!(
+        "https://api.telegram.org/bot{apiKey}/sendMessage?chat_id={chat_id}&parse_mode=HTML&text=<code>{text}</code>",
+        apiKey = env::var("TELEGRAM_MICHELED_PLUTO_API_KEY").unwrap(),
+        chat_id = env::var("TELEGRAM_MICHELED_PLUTO_CHAT_ID").unwrap(),
+        text = text
+    ))
+    .call();
+}
 
 /// Perform a dns lookup for the given domain.
-///
-/// * `resolver` - A valid instance to perform DNS lookups
-/// * `domain` - The domain to resolve to an IP address
 fn lookup_domain(resolver: &Resolver, domain: &str) {
     let resolve_result = resolver.lookup_ip(domain);
 
-    if resolve_result.is_err() {
-        println!("{:?}", resolve_result.unwrap_err().kind());
+    if resolver.lookup_ip(domain).is_err() {
+        send_telegram_message(format!("{:?}", resolve_result.unwrap_err().kind()));
     }
 }
 
 /// Given a list of domains, execute a DNS lookup.
-///
-/// * `domains` - The list of domains
 fn validate_domains(domains: Vec<&str>) {
     let resolver = Resolver::from_system_conf().unwrap();
 
@@ -34,12 +42,11 @@ fn main() -> Result<(), Error> {
     let domains = vec![
         "annikaschall.com",
         "micheledurante.co.uk",
-        "micheledurante",
         "micheledurante.com",
     ];
 
     validate_domains(domains);
-    Ok(())
+    Ok(send_telegram_message(String::from("DNS records ok")))
 }
 
 #[cfg(test)]
@@ -51,42 +58,36 @@ mod tests {
     use trust_dns_resolver::error::ResolveResult;
     use trust_dns_resolver::lookup_ip::LookupIp;
 
-    #[automock]
-    trait ResolverTrait {
-        fn lookup_ip(&self, host: &str) -> ResolveResult<LookupIp>;
-    }
-
-    #[automock]
     trait MyTrait {
         fn foo(&self, x: u32) -> u32;
     }
 
     #[test]
     fn my_test() {
-        let mut mock = MockMyTrait::new();
-        mock.expect_foo().with(eq(4)).times(1).returning(|x| x + 1);
+        // let mut mock = MockMyTrait::new();
+        mock! {
+            MyTrait {}     // Name of the mock struct, less the "Mock" prefix
+            trait Clone {   // definition of the trait to mock
+                fn clone(&self) -> Self;
+            }
+        }
+
+        let mut mock1 = MockMyStruct::new();
+        let mut mock = MockMyStruct::new();
+        let mut mock = mock.expect_foo().with(eq(4)).times(1).returning(|x| x + 1);
         assert_eq!(5, mock.foo(4));
     }
 
-    #[test]
-    fn test_lookup_domain_empty_string() {
-        let mut mock = MockResolverTrait::new();
-        let domain = "invalid-domain";
-
-        mock.expect_lookup_ip()
-            .with(domain)
-            .times(1)
-            .returning(|x| x);
-
-        assert_eq!(domain, mock.expect_lookup_ip(domain));
-    }
-
-    #[test]
-    fn test_lookup_domain_invalid() {}
-
-    #[test]
-    fn test_lookup_domain_valid() {}
-
-    #[test]
-    fn test_validate_domains_invalid() {}
+    // #[test]
+    // fn test_lookup_domain_empty_string() {
+    //     let mut mock = MockResolver::new();
+    //     let domain = "invalid-domain";
+    //
+    //     mock.expect_lookup_ip()
+    //         .with(eq(domain))
+    //         .times(1)
+    //         .returning(ResolveResult::Ok(()));
+    //
+    //     mock.expect_lookup_ip(domain);
+    // }
 }
